3fd7112604af74e10678f5d863a91c233aeecb4d
Vladimir Armantovich <ruff-its-me@users.noreply.github.com>
2017-10-20 11:28:30 +0600

    Test name changed cause maven-surefire-plugin searches for "*Test", spring boot parent that behaviour to "*Tests", but we not use parent pom.xml. Alternative case - properly configure maven-surefire-plugin manually in own pom.xml.
    https://stackoverflow.com/questions/25609672/spring-boot-no-maven-parent-and-unittests-not-running

--- src/test/java/com/example/calculator/CalculatorApplicationTest.java
+++ src/test/java/com/example/calculator/CalculatorApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CalculatorApplicationTests {
+public class CalculatorApplicationTest {
 
 	@Test
 	public void contextLoads() {
